3b18bf2d34687d2ac1e742b5ec709e9bc3d5d8f6
Mike Chau <mike@chau.io>
2015-07-08 14:29:41 -0500

    update eslint config and added a karma test

--- test/components/Application.test.js
+++ test/components/Application.test.js
@@ -1,2 +1,13 @@
+import React from 'react';
+import ReactTestUtils from 'react/lib/ReactTestUtils';
 import Application from '../../src/components/Application';
 
+describe('Application', () => {
+  it('renders a div', () => {
+    const shallowRenderer = ReactTestUtils.createRenderer();
+    shallowRenderer.render(<Application />);
+
+    const component = shallowRenderer.getRenderOutput();
+    expect(component.type).to.equal('div');
+  });
+});
